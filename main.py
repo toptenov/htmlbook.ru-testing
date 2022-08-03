e76eccb0f6b83b3448dc37ebee5ade1607427181
toptenov <toptenov@gmail.com>
2022-08-03 12:18:07 +0300

    Add if __name__ == "__main__" and Explicit Wait

--- main.py
+++ main.py
@@ -1,35 +1,43 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
 import time
 
-link = "http://htmlbook.ru/"
+def main():
+    link = "http://htmlbook.ru/"
 
-options = webdriver.ChromeOptions()
-options.add_argument("--disable-blink-features")
-options.add_experimental_option("excludeSwitches", ["enable-automation"])
-options.add_experimental_option('useAutomationExtension', False)
-options.add_argument('--disable-extensions')
-options.add_argument("--incognito")
-options.add_argument("--disable-plugins-discovery")
-options.add_argument("--start-maximized")
-options.add_argument('--ignore-certificate-errors')
-options.add_argument('--ignore-ssl-errors')
-options.add_experimental_option('excludeSwitches', ['enable-logging'])
-browser = webdriver.Chrome(options=options)
-browser.execute_cdp_cmd(
-    "Page.addScriptToEvaluateOnNewDocument",
-    {"source": "Object.defineProperty(navigator, 'webdriver', {get: () => undefined})"}
-)
-browser.execute_cdp_cmd(
-    "Page.addScriptToEvaluateOnNewDocument",
-    {"source": "const newProto = navigator.__proto__ delete newProto.webdriver navigator.__proto__ = newProto"}
-)
+    options = webdriver.ChromeOptions()
+    options.add_argument("--disable-blink-features")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('useAutomationExtension', False)
+    options.add_argument('--disable-extensions')
+    options.add_argument("--incognito")
+    options.add_argument("--disable-plugins-discovery")
+    options.add_argument("--start-maximized")
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--ignore-ssl-errors')
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    browser = webdriver.Chrome(options=options)
+    browser.execute_cdp_cmd(
+        "Page.addScriptToEvaluateOnNewDocument",
+        {"source": "Object.defineProperty(navigator, 'webdriver', {get: () => undefined})"}
+    )
+    browser.execute_cdp_cmd(
+        "Page.addScriptToEvaluateOnNewDocument",
+        {"source": "const newProto = navigator.__proto__ delete newProto.webdriver navigator.__proto__ = newProto"}
+    )
 
-try:
-    browser.get(link)
-    time.sleep(3)
-    button = browser.find_element(By.XPATH, '//a[@href="/content"]')
-    button.click()
-    time.sleep(3)
-finally:
-    browser.quit()
+    try:
+        browser.get(link)
+        button = WebDriverWait(browser, 5).until(
+            expected_conditions.element_to_be_clickable(
+                (By.XPATH, '//a[@href="/content"]')
+            )
+        )
+        button.click()
+    finally:
+        browser.quit()
+
+if __name__ == "__main__":
+    main()
